6c90bbe9e84f870948e5eddfc0fb2b3776e63918
Patrick Araujo <rayssacoosta@gmail.com>
2021-04-12 12:33:06 -0300

    Função próxima página

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const requisicao = fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1")
-.then(r => r.json())
-.then(data =>{
-        const html = data.products.map( (product) => {
-            return `
-                <div class="produto">
-                    <img src=http:${product.image} class="img-product">
-                    <p class="name-product">${product.name}</p>
-                    <p class="desc-product">${product.description}</p>
-                    <p class="oldprice-product">${product.oldPrice}</p>
-                    <p class="price-product">${product.price}</p>
-                    <p class="payment-product">ou ${product.installments.count}x de R$${product.installments.value}</p>
-                    <button class="btn-product">Comprar</button>
-                </div>
-            `
-        }
-    );
-    document.querySelector("#produto").innerHTML = html;
-})
+var nextPage = ""
+var url = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
+
+function showProducts(url){
+    fetch(url)
+    .then(r => r.json())
+    .then(data =>{
+            const html = data.products.map( (product) => {
+                return `
+                    <div class="produto">
+                        <img src=http:${product.image} class="img-product">
+                        <p class="name-product">${product.name}</p>
+                        <p class="desc-product">${product.description}</p>
+                        <p class="oldprice-product">${product.oldPrice}</p>
+                        <p class="price-product">${product.price}</p>
+                        <p class="payment-product">ou ${product.installments.count}x de R$${product.installments.value}</p>
+                        <button class="btn-product">Comprar</button>
+                    </div>
+                `
+            },
+            nextPage = "https://" + data.nextPage
+        );
+        document.querySelector("#produto").innerHTML = html;
+    })
+
+}
+
+function moreProduct(){
+    showProducts(nextPage)
+}
+
+showProducts(url);
